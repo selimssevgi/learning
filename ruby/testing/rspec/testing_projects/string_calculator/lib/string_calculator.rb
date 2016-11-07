d7cadb844fd082958ed0ff03d82288ff93ef0c6e
selimssevgi <selimssevgi@gmail.com>
2016-11-07 08:59:26 +0200

    rspec tests

---
+++ ruby/testing/rspec/testing_projects/string_calculator/lib/string_calculator.rb
@@ -0,0 +1,11 @@
+class StringCalculator
+
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(",").map { |num| num.to_i }
+      numbers.inject(0) { |sum, number| sum + number }
+    end
+  end
+end
